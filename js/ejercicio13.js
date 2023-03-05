eaf6562b29e9fab150d64e602d92716ae44d3bbc
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-03-05 20:16:07 -0300

    ejercicio13 realizado

---
+++ js/ejercicio13.js
@@ -0,0 +1,6 @@
+/*
+13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+*/
+
+let cadenaTexto = prompt('Introduce la cadena de texto:');
+document.write(`Texto en Mayúsculas: ${cadenaTexto.toUpperCase()}`);
