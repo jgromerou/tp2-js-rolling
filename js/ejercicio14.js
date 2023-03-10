94c5d09769e6a5b1f1f4ed6bb2052791a32ef8e4
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-03-10 08:46:23 -0300

    se cambió de let a const la variable cadenaTexto

--- js/ejercicio14.js
+++ js/ejercicio14.js
@@ -3,7 +3,7 @@
 entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, 
 deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 */
-let cadenaTexto = prompt('Ingrese la cadena de texto: ');
+const cadenaTexto = prompt('Ingrese la cadena de texto: ');
 
 for (let i = 0; i < cadenaTexto.length; i++) {
   if (i === cadenaTexto.length - 1) {
